481a0cf2bd51d01244f1cefffec6e6a2bf2e0cef
Fatima khurram <149939834+fatimakhurram@users.noreply.github.com>
2024-10-18 12:48:55 +0500

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
   name: "Fatima Khurram",
   profile: '/profile.png',
   designation: "DevOps Engineer&Cybersecurity expert",
-  description: "My name is Fatima Khurram. I am a professional and cybersecurity enthusiast in my daily life. I am a quick learner with a self-learning attitude. I am passionate about learning and exploring new technologies, with a strong focus on cybersecurity and DevOps. I enjoy tackling complex problems and continuously seek ways to enhance security and optimize processes. My core skills revolve around JavaScript, and I leverage it for various tasks in both fields. I am open to job opportunities that align with my skills and passion for making technology more secure and efficient..",
+  description: "My name is Fatima Khurram. I am a professional DevOps Engineer and cybersecurity enthusiast in my daily life. I am a quick learner with a self-learning attitude. I am passionate about learning and exploring new technologies, with a strong focus on cybersecurity and DevOps. I enjoy tackling complex problems and continuously seek ways to enhance security and optimize processes. My core skills revolve around JavaScript, and I leverage it for various tasks in both fields. I am open to job opportunities that align with my skills and passion for making technology more secure and efficient..",
   email: 'khurramfatima201@gmail.com',
   address: 'Lahore, Pakistan ',
   github: 'https://github.com/fatimakhurram',
